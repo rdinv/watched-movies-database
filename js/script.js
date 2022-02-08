1481869a1fa994e8e37c5f4e905c9382e709b3b3
rdinv <radone21@gmail.com>
2022-02-08 21:20:47 +0200

    loop and condition add

--- js/script.js
+++ js/script.js
@@ -8,12 +8,30 @@ const personalMovieDatabase = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов', ''),
-      b = prompt('На сколько оцените его', 'от 1 до 10'),
-      c = prompt('Один из последних просмотренных фильмов', ''),
-      d = prompt('На сколько оцените его', 'от 1 до 10');
+for (let i = 0; i < 2; i++){
+    const a = prompt('Один из последних просмотренных фильмов',
+         'The Shawshank Redemption'),
+          b = prompt('На сколько оцените его', 'form 1 to 10');
 
-personalMovieDatabase.movies[a] = b;
-personalMovieDatabase.movies[c] = d;
+
+    if (a != null && b !=null && a != '' && b != '' && a.length < 50){
+        personalMovieDatabase.movies[a] = b;
+        console.log('Done');
+    } else {
+        console.log('Error');
+        i--;
+    }
+}
+
+if (personalMovieDatabase.count < 10) {
+    console.log('You watched few movies');
+} else if (personalMovieDatabase.count >= 10 &&
+    personalMovieDatabase.count < 30) {
+    console.log('You are a great viewer');
+} else if (personalMovieDatabase.count >= 30){
+    console.log('You are a movie buff');
+} else {
+    console.log('Error');
+}
 
 console.log(personalMovieDatabase);
