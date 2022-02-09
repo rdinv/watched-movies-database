bf057162fbb6e70d8ebe4b2067e419453c00d7ce
rdinv <radone21@gmail.com>
2022-02-09 23:51:56 +0200

    function add. personalDB add

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-const numbetOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+let numbetOfFilms;
+
+function start (){
+    numbetOfFilms = +prompt('How many films have you already watched?', '');
+
+    while (numbetOfFilms == '' || numbetOfFilms == null || isNaN(numbetOfFilms)) {
+        numbetOfFilms = +prompt('How many films have you already watched?', '');
+    }
+}
+start();
 
 const personalMovieDatabase = {
     count: numbetOfFilms,
@@ -8,30 +17,40 @@ const personalMovieDatabase = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++){
-    const a = prompt('Один из последних просмотренных фильмов',
-         'The Shawshank Redemption'),
-          b = prompt('На сколько оцените его', 'form 1 to 10');
-
-
-    if (a != null && b !=null && a != '' && b != '' && a.length < 50){
-        personalMovieDatabase.movies[a] = b;
-        console.log('Done');
-    } else {
-        console.log('Error');
-        i--;
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++){
+        const a = prompt('One of the last movies you watched',
+             'The Shawshank Redemption'),
+              b = prompt('How much would you rate it', 'form 1 to 10');
+    
+        if (a != null && b !=null && a != '' && b != '' && a.length < 50){
+            personalMovieDatabase.movies[a] = b;
+            console.log('Done');
+        } else {
+            console.log('Error');
+            i--;
+        }
     }
 }
+rememberMyFilms();
 
-if (personalMovieDatabase.count < 10) {
-    console.log('You watched few movies');
-} else if (personalMovieDatabase.count >= 10 &&
-    personalMovieDatabase.count < 30) {
-    console.log('You are a great viewer');
-} else if (personalMovieDatabase.count >= 30){
-    console.log('You are a movie buff');
-} else {
-    console.log('Error');
+function detectPersonalLevel() {
+    if (personalMovieDatabase.count < 10) {
+        console.log('You watched few movies');
+    } else if (personalMovieDatabase.count >= 10 &&
+        personalMovieDatabase.count < 30) {
+        console.log('You are a great viewer');
+    } else if (personalMovieDatabase.count >= 30){
+        console.log('You are a movie buff');
+    } else {
+        console.log('Error');
+    }
 }
+detectPersonalLevel();
 
-console.log(personalMovieDatabase);
+function showMyDB (hidden) {
+    if (!hidden){
+        console.log(personalMovieDatabase);
+    }
+}
+showMyDB(personalMovieDatabase.privat);
